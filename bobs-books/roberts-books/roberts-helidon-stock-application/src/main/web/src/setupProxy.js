28c8ecdfc70634efbaccb6eab57f245586d4b5b1
Ravi Singhal <ravi.r.singhal@oracle.com>
2020-06-23 18:01:17 +0530

    working changes

--- bobs-books/roberts-books/roberts-helidon-stock-application/src/main/web/src/setupProxy.js
+++ bobs-books/roberts-books/roberts-helidon-stock-application/src/main/web/src/setupProxy.js
@@ -5,5 +5,5 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('/api/v2/span', { target: 'http://zipkin.istio-system:9411/' }));
     //Uncomment when running in development webpack server (npm start)
-    //app.use(proxy('/api', { target: 'http://localhost:8080/' }));
+    app.use(proxy('/api', { target: 'http://localhost:8080/' }));
   };
