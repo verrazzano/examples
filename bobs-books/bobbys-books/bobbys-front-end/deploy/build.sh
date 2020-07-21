0def28e97c7fbb475c781d0cfd43812125caa577
Mark Nelson <mark.x.nelson@oracle.com>
2020-07-21 17:23:34 -0400

    test db pwd enc fix
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- bobs-books/bobbys-books/bobbys-front-end/deploy/build.sh
+++ bobs-books/bobbys-books/bobbys-front-end/deploy/build.sh
@@ -83,9 +83,9 @@ imagetool.sh create \
     --jdkVersion 8u251 \
     --additionalBuildCommands additional-build-commands \
     --wdtModel bobbys-front-end.yaml \
-    --wdtModelOnly \
     --wdtArchive archive.zip \
-    --wdtDomainHome /u01/oracle/user_projects/domains/bobbys-front-end
+    --wdtDomainHome /u01/oracle/user_projects/domains/bobbys-front-end #\
+    #--wdtModelOnly \
 
 
 
