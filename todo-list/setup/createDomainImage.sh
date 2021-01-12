ee95c380d1da8ed75e3e40b8203c27a0e4699b67
Will Hopkins <wmhopkins@users.noreply.github.com>
2021-01-12 15:52:32 -0500

    update wdt_domain.yaml to use model tokens, add Dockerfile and createDomainImage.sh (#37)

---
+++ todo-list/setup/createDomainImage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Copyright (c) 2020, 2021 Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+
+docker build --build-arg 'WDT_MODEL=wdt_domain.yaml' --build-arg 'WDT_ARCHIVE=wdt_archive.zip' -t todo-domain .
