2e787d86ed62467f8b7a4043581a32ad123630ca
Aleks Seovic <aleks@seovic.com>
2021-01-27 17:24:06 -0500

    Switch to Jib to create Docker image that works with Coherence Operator 3.x

--- bobs-books/roberts-books/roberts-coherence/build.sh
+++ bobs-books/roberts-books/roberts-coherence/build.sh
@@ -2,7 +2,5 @@
 # Copyright (c) 2020, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-TAG=${1:-test}
-
 mvn clean install
-docker build -t ghcr.io/verrazzano/example-roberts-coherence:${TAG} .
+mvn package -Pdocker
