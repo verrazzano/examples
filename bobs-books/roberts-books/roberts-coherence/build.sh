4e1e795113ffcf112b0c78bd20fcea2cac68e014
Aleks Seovic <aleks@seovic.com>
2021-01-28 00:47:33 -0500

    Revert "Switch to Jib to create Docker image that works with Coherence Operator 3.x"
    This reverts commit 2e787d86

--- bobs-books/roberts-books/roberts-coherence/build.sh
+++ bobs-books/roberts-books/roberts-coherence/build.sh
@@ -2,5 +2,7 @@
 # Copyright (c) 2020, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+TAG=${1:-test}
+
 mvn clean install
-mvn package -Pdocker
+docker build -t ghcr.io/verrazzano/example-roberts-coherence:${TAG} .
