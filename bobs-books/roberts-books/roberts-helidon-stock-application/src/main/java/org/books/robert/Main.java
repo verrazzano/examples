c9eeab1b433ab08a3785af1d1529ea5e0de0c689
Supriya Kalyanasundaram <supriya.kalyanasundaram@oracle.com>
2021-04-13 16:39:57 +0000

    vz-2258

--- bobs-books/roberts-books/roberts-helidon-stock-application/src/main/java/org/books/robert/Main.java
+++ bobs-books/roberts-books/roberts-helidon-stock-application/src/main/java/org/books/robert/Main.java
@@ -4,6 +4,7 @@
 package org.books.robert;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -50,7 +51,8 @@ public final class Main {
      */
     private static void setupLogging() throws IOException {
         // load logging configuration
-        LogManager.getLogManager().readConfiguration(
-                Main.class.getResourceAsStream("/logging.properties"));
+        try (InputStream is = Main.class.getResourceAsStream("/logging.properties")) {
+           LogManager.getLogManager().readConfiguration(is);
+        }
     }
 }
