4233ee29240e711697bc8dbfc2c6c1a406d7f491
Guoyong Zhang <guoyong.zhang@oracle.com>
2021-05-12 15:24:15 -0400

    use imagetool instead of dockerfile

--- todo-list/setup/build.sh
+++ todo-list/setup/build.sh
@@ -20,8 +20,49 @@ mvn -B clean package -Dconfiguration=../../exporter-config.yaml
 cd ../..
 mkdir -p ./wlsdeploy/applications
 cp weblogic-monitoring-exporter/webapp/target/wls-exporter.war ./wlsdeploy/applications/wls-exporter.war
+
 cp ../LICENSE.txt .
 cp ../THIRD_PARTY_LICENSES.txt .
 
-./createArchive.sh
-./createDomainImage.sh $1
+echo 'Build the WDT archive...'
+rm wdt_archive.zip
+mkdir -p ./wlsdeploy/applications
+cp ../target/todo.war ./wlsdeploy/applications
+zip -r wdt_archive.zip wlsdeploy
+rm -r wlsdeploy
+
+echo 'Download WDT...'
+if [ -f weblogic-deploy.zip ]; then
+    echo 'Using existing weblogic-deploy.zip...'
+else
+    echo 'Downloading weblogic-deploy.zip...'
+    wget https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.9.0/weblogic-deploy.zip
+fi
+
+echo 'Download WebLogic Image Tool...'
+if [ -f imagetool.zip ]; then
+    echo 'Using existing imagetool.zip...'
+else
+    echo 'Downloading imagetool.zip...'
+    wget https://github.com/oracle/weblogic-image-tool/releases/download/release-1.9.6/imagetool.zip
+    unzip imagetool.zip
+fi
+
+export PATH=`pwd`/imagetool/bin:$PATH
+
+echo 'Add installers to Image Tool cache...'
+imagetool.sh cache addInstaller --type jdk --version 8u261 --path ${JDK8_BUNDLE}
+imagetool.sh cache addInstaller --type wls --version 12.2.1.4.0 --path ${WEBLOGIC_BUNDLE}
+imagetool.sh cache addInstaller --type wdt --version latest --path weblogic-deploy.zip
+
+echo 'Create image with domain...'
+imagetool.sh create \
+    --tag $1 \
+    --version 12.2.1.4.0 \
+    --jdkVersion 8u261 \
+    --fromImage container-registry.oracle.com/os/oraclelinux:7-slim@sha256:84433cf4f605c35fa032ff87d2635c3ab5aaa7fbdb4bb8f90e60f4ab1b96d371 \
+    --wdtModel wdt_domain.yaml \
+    --wdtArchive wdt_archive.zip \
+    --wdtDomainHome /u01/oracle/user_projects/domains/tododomain \
+    --wdtModelOnly \
+    --additionalBuildCommands imagetool-additions
