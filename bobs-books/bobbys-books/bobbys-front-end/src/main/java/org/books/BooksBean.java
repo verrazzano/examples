a31a073c277b48eb5c5d2ccb4b4aaaeb8e921c59
vicky.sarkar@oracle.com <vsarkar@vsarkar-1.subnet2ad3phx.devweblogicphx.oraclevcn.com>
2021-12-07 14:45:35 +0000

    testing changes

--- bobs-books/bobbys-books/bobbys-front-end/src/main/java/org/books/BooksBean.java
+++ bobs-books/bobbys-books/bobbys-front-end/src/main/java/org/books/BooksBean.java
@@ -171,7 +171,9 @@ public class BooksBean implements Serializable {
     private void reload() {
         try {
             ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-            ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
+            String url = ((HttpServletRequest) ec.getRequest()).getRequestURL().toString().replace("http://","https://");
+            logger.info("ec: " + url);
+            ec.redirect(url);
         } catch (Exception e) {
 			logger.error("failed to refresh page", e);
         }
