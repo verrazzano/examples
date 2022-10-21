19d2546504e657d693d07cf2f237494794c0b4c1
alyssavlev <alyssa.l.levine@oracle.com>
2022-10-21 16:41:49 +0000

    added file NotFoundExceptionMapper.java to debug and serve as an exception mapper

---
+++ bobs-books/roberts-books/roberts-helidon-stock-application/src/main/java/org/books/robert/NotFoundExceptionMapper.java
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package org.books.robert; 
+
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<Exception> {
+  public Response toResponse(Exception ex) {
+	System.out.println (“in exception mapper - exception type is: “ + ex);
+    return Response.temporaryRedirect(new URI("/")).build();
+  }
+}
