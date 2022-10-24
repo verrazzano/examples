d4df782441c010802d98a59c5d17a63eea4eb321
alyssavlev <alyssa.l.levine@oracle.com>
2022-10-24 20:25:58 +0000

    fixed console error - added library

--- bobs-books/roberts-books/roberts-helidon-stock-application/src/main/java/org/books/robert/NotFoundExceptionMapper.java
+++ bobs-books/roberts-books/roberts-helidon-stock-application/src/main/java/org/books/robert/NotFoundExceptionMapper.java
@@ -6,11 +6,12 @@ package org.books.robert;
 import javax.ws.rs.ext.Provider;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 
 @Provider
 public class NotFoundExceptionMapper implements ExceptionMapper<Exception> {
   public Response toResponse(Exception ex) {
 	System.out.println("in exception mapper - exception type is: " + ex);
-    //return Response.temporaryRedirect(new URI("/")).build();
+    return Response.temporaryRedirect(new URI("/")).build();
   }
 }
